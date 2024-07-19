fb6996bad774c67f24e7edb3459bed9e4ca8a3b0
Riley <realbewhy@gmail.com>
2024-07-19 16:51:16 +0700

    -- update

--- src/controllers/s3.controller.ts
+++ src/controllers/s3.controller.ts
@@ -13,26 +13,31 @@ const upload: RequestHandler = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { verifyUser } = req.body;
-  return s3Upload(req, res, function (err: any) {
-    if (err) {
-      console.log(err);
-      if (err.code === "LIMIT_FILE_SIZE") {
-        return res.status(400).json({
-          errors: [
-            {
-              code: "LIMIT_FILE_SIZE",
-              message: `File size limit exceeded. Maximum: ${maxFileSize}MB`,
-            },
-          ],
-        });
+  try {
+    const { verifyUser } = req.body;
+    return s3Upload(req, res, function (err: any) {
+      if (err) {
+        console.log(err);
+        if (err.code === "LIMIT_FILE_SIZE") {
+          return res.status(400).json({
+            errors: [
+              {
+                code: "LIMIT_FILE_SIZE",
+                message: `File size limit exceeded. Maximum: ${maxFileSize}MB`,
+              },
+            ],
+          });
+        }
+        return res.status(500);
       }
-      return res.status(500);
-    }
-    const uploadedImages = req.files.map((file: any) => file.location);
-    uploadImageModel.multiCreate(uploadedImages, verifyUser.id);
-    res.status(200).json(uploadedImages);
-  });
+      const uploadedImages = req.files.map((file: any) => file.location);
+      uploadImageModel.multiCreate(uploadedImages, verifyUser.id);
+      res.status(200).json(uploadedImages);
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
 export const s3Controller = {
